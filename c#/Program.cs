1cc22ab46573fef54b05ecc963f4290700b9986a
Rawan Lucas <Rawanc.silva@gmail.com>
2022-09-07 08:54:53 -0300

    exercicios

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,8 +1,12 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+
+internal class NewBaseType
 {
-   static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("aprendendo c#");
+       string cor_do_carro ="";
+       Console.WriteLine("Qual a cor do carro?");
+       cor_do_carro = Console.ReadLine();
+       Console.WriteLine("A cor do carro é " + cor_do_carro);
     }
 }
