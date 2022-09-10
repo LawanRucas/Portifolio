c1a5bbdc0007271cfc9172615181ea4b60bb201d
Rawan Lucas <Rawanc.silva@gmail.com>
2022-09-10 18:52:46 -0300

    aprendendo

---
+++ CSharp/Exercicio1.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("Qual o seu nome?");
+        var nome = Console.ReadLine();
+        Console.WriteLine("Qual a sua idade?");
+        var idade = Console.ReadLine();
+        Console.WriteLine("sua idade é {0} anos e seu nome é {1}", idade, nome);
---
+++ c#/Program.cs
@@ -0,0 +1,24 @@
+﻿
+        float nota1 = 0;
+        float nota2 = 0;
+        float nota3 = 0;
+
+        Console.WriteLine("vou calcular a media de 3 notas");
+        Console.WriteLine("Qual a sua nota 1?");
+        nota1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Qual a sua nota 2?");
+        nota2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Qual a sua nota 3?");
+        nota3 = Convert.ToInt32(Console.ReadLine());
+
+        var media = (nota1 + nota2 + nota3) / 3;
+
+        if (media >= 7)
+        {
+            Console.WriteLine("Aprovado");
+        }
+        else
+        {
+            Console.WriteLine("Reprovado");
+        }
+        Console.WriteLine(4%2);
